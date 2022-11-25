75fe36796209331ecb7287a5e8bea3bd51458278
Luka Bracanovic <lbracano@gmail.com>
2022-11-25 23:53:01 +0100

    Create Product crud endpoints

---
+++ packages/server/src/product/routes.ts
@@ -0,0 +1,9 @@
+import app from "../app";
+
+app.get("/product/:id", async (req, res) => {});
+
+app.post("/product/:id", async (req, res) => {});
+
+app.put("/product/:id", async (req, res) => {});
+
+app.delete("/product/:id", async (req, res) => {});

1a00c753532c1653fcb2281492fb2af6208d9723
Luka Bracanovic <lbracano@gmail.com>
2022-11-25 23:53:14 +0100

    Create User crud endpoints

---
+++ packages/server/src/user/routes.ts
@@ -0,0 +1,9 @@
+import app from "../app";
+
+app.get("/user/:id", async (req, res) => {});
+
+app.post("/user/:id", async (req, res) => {});
+
+app.put("/user/:id", async (req, res) => {});
+
+app.delete("/user/:id", async (req, res) => {});

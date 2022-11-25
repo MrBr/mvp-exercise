edda02c53d83d6af8dfe063e690e0630463a53c5
Luka Bracanovic <lbracano@gmail.com>
2022-11-25 23:49:58 +0100

    Setup express instance

---
+++ packages/server/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "2mb" }));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Application started at port: ${process.env.PORT}`)
+);
+
+export default app;

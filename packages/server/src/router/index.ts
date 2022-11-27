73e66f6de60b48044646d5888aafaab70716f7b8
Luka Bracanovic <lbracano@gmail.com>
2022-11-27 23:52:58 +0100

    Add server cors

--- packages/server/src/router/index.ts
+++ packages/server/src/router/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const router = express();
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json({ limit: "2mb" }));
+router.use(cors());
+
+console.log(process.env.APP_DOMAIN);
 
 router.listen(process.env.PORT, () =>
   console.log(`Application started at port: ${process.env.PORT}`)

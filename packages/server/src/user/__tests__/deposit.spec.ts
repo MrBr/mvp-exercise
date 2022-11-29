30f82c27873ebb466db1f033ad581f32343c8132
Luka Bracanovic <lbracano@gmail.com>
2022-11-29 20:52:42 +0100

    Add deposit tests

--- packages/server/src/user/__tests__/deposit.spec.ts
+++ packages/server/src/user/__tests__/deposit.spec.ts
@@ -5,7 +5,7 @@ import { InvalidDepositError } from "../errors";
 import { generateToken } from "../../auth";
 import { jsonHeaders, startTestDbContainer } from "../../test";
 
-import { buyer_0 } from "../fixtures";
+import { buyer_0, seller } from "../fixtures";
 import { seedUsers } from "../seeders";
 
 describe("PUT /deposit", () => {
@@ -13,7 +13,7 @@ describe("PUT /deposit", () => {
   beforeEach(async () => {
     stopContainer = await startTestDbContainer();
 
-    await seedUsers([buyer_0]);
+    await seedUsers([buyer_0, seller]);
   });
 
   afterEach(async () => {
@@ -31,6 +31,7 @@ describe("PUT /deposit", () => {
     expect(depositResponse.statusCode).toEqual(200);
     expect(depositResponse.body.data).toEqual(5);
   });
+
   it("returns invalid value error", async () => {
     const token = generateToken({ userId: buyer_0.id });
 
@@ -44,22 +45,55 @@ describe("PUT /deposit", () => {
     expect(depositResponse.body.error).toEqual(errorMessage);
   });
 
-  it("returns forbidden request status", async () => {
+  it("returns forbidden request when trying to deposit on different account", async () => {
+    const token = generateToken({ userId: 100 });
+
     const depositResponse = await request(router)
       .put("/deposit")
-      .set(jsonHeaders())
-      .send({ coins: 6 });
+      .set(jsonHeaders(token))
+      .send({ coins: 5 });
 
     expect(depositResponse.statusCode).toEqual(401);
   });
 
-  it("returns bad request status", async () => {
+  it("returns forbidden request status without token", async () => {
+    const depositResponse = await request(router)
+      .put("/deposit")
+      .set(jsonHeaders())
+      .send({ coins: 5 });
+
+    expect(depositResponse.statusCode).toEqual(401);
+  });
+
+  it("returns bad request status without data", async () => {
     const token = generateToken({ userId: buyer_0.id });
 
     const depositResponse = await request(router)
       .put("/deposit")
       .set(jsonHeaders(token))
       .send();
+
+    expect(depositResponse.statusCode).toEqual(400);
+  });
+
+  it("returns bad request status with wrong data type", async () => {
+    const token = generateToken({ userId: buyer_0.id });
+
+    const depositResponse = await request(router)
+      .put("/deposit")
+      .set(jsonHeaders(token))
+      .send({ coins: "5" });
+
+    expect(depositResponse.statusCode).toEqual(400);
+  });
+
+  it("returns bad request status for seller role", async () => {
+    const token = generateToken({ userId: seller.id });
+
+    const depositResponse = await request(router)
+      .put("/deposit")
+      .set(jsonHeaders(token))
+      .send({ coins: 5 });
 
     expect(depositResponse.statusCode).toEqual(400);
   });

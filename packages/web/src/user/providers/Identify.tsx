17189e37d0a2b272dcc8eeb751fde13fce836afe
Luka Bracanovic <lbracano@gmail.com>
2022-11-30 10:32:28 +0100

    Implement auto log out

--- packages/web/src/user/providers/Identify.tsx
+++ packages/web/src/user/providers/Identify.tsx
@@ -1,8 +1,27 @@
 import React, { PropsWithChildren, useEffect } from "react";
 import { useUserAuth } from "../hooks";
 
+const { fetch: originalFetch } = window;
+
 const Identify = ({ children }: PropsWithChildren) => {
-  const { fetchMe } = useUserAuth();
+  const { fetchMe, logout } = useUserAuth();
+
+  useEffect(() => {
+    window.fetch = async (...args) => {
+      let [resource, config] = args;
+
+      let response = await originalFetch(resource, config);
+
+      if (response.status === 401) {
+        logout();
+      }
+
+      return response;
+    };
+    return () => {
+      window.fetch = originalFetch;
+    };
+  });
 
   // Check if user is logged in on mount
   useEffect(() => {
